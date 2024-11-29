18b82a8402898d697fdb8f6661b941896512bf0b
JamesFrench <119378901+JamesMinoucha@users.noreply.github.com>
2024-11-29 17:57:23 +0100

    Setup for Pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dispy',
+    name='dispy-bot',
     version='0.1.0',
-    description='A python-coded discord bot library',
+    description='A python-coded discord bot library.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='James French',
